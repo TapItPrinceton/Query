08e68c07777866c906d46a8d0eca112d88b2042c
Joshin <JoshuaShin96@gmail.com>
2014-11-15 11:38:08 -0500

    Made some changes to the progress bar

--- app/src/main/java/com/example/josh/tapit/MainActivity.java
+++ app/src/main/java/com/example/josh/tapit/MainActivity.java
@@ -1,61 +1,42 @@
 package com.example.josh.tapit;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.ProgressBar;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 
 public class MainActivity extends Activity {
 
     protected ProgressBar mProgressBar;
+    private int current;
+    private static final int MAX = 100; // change when we know class size, or keep as a percentage
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-        mProgressBar.setVisibility(View.VISIBLE);
-        final Button updateButton = (Button) findViewById(R.id.button);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar); // make the progress bar
+        mProgressBar.setVisibility(View.VISIBLE); // not sure if necessary, but makes the bar visible
+        mProgressBar.setMax(MAX);
+
+        final Button updateButton = (Button) findViewById(R.id.button); // make the button
 
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                current++; // increment count
+                mProgressBar.setProgress(current);
 
             }
         };
         updateButton.setOnClickListener(listener);
 
-    }
-
-    protected void OnItemClickListener()
-
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        try {
-            JSONArray jsonPosts = mBlogData.getJSONArray("posts");
-            JSONObject jsonPost = jsonPosts.getJSONObject(position);
-            String blogUrl = jsonPost.getString("url");
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(blogUrl));
-            startActivity(intent);
-        }
-        catch (JSONException e) {
-            logException(e);
-        }
     }
 
 
