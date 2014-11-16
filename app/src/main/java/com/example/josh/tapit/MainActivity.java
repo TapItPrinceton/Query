3ad41ac325fb4a7fb840356d44ddcb027eda6a83
Joshin <JoshuaShin96@gmail.com>
2014-11-16 03:44:53 -0500

    last few changes

--- app/src/main/java/com/example/josh/tapit/MainActivity.java
+++ app/src/main/java/com/example/josh/tapit/MainActivity.java
@@ -6,9 +6,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
@@ -32,20 +34,23 @@ import java.util.Date;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GestureDetector.OnGestureListener {
 
     protected ProgressBar mProgressBar;
     protected TextView status;
     protected Button updateButton; // button to change state
     protected ListView listView; // listview to hold top questions
+    protected TextView queueView;
     private RelativeLayout relativeLayout;
     private static final int MAX = 100; // change when we know class size
     private boolean confused = true; // true = confused, false = not confused
     private String class_name = "";
-    private static final int GREEN = Color.parseColor("#228822");
+    private static final int GREEN = Color.parseColor("#4dff38");
     private static final int RED = Color.parseColor("#FF4500");
-    private static Date tempDate;
-    private static final int Q_DURATION = 30;
+    private static Date lastDate;
+    private static final int Q_DURATION = 12000000;
+    private GestureDetector mDetector;
+    private ArrayList<String[]> Question_queue;
 
     //======================
     private int mInterval = 500; // 0.5 seconds
@@ -56,12 +61,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // set tempDate
         Calendar cal = Calendar.getInstance();
-        cal.set(2014, Calendar.NOVEMBER, 16); //Year, month and day of month
-        tempDate = cal.getTime();
+        lastDate = cal.getTime();
 
-
+        Question_queue = new ArrayList<String[]>();
 
         // Starts LoginActivity class
         Intent resultIntent = new Intent(this, LoginActivity.class);
@@ -86,8 +89,13 @@ public class MainActivity extends Activity {
         updateButton.setText(getString(R.string.button_text));
 
         // Sets the initial status to positive
-        status = (TextView) findViewById(R.id.textView);
-        status.setText(getString(R.string.status_positive));
+        //status = (TextView) findViewById(R.id.textView);
+        //status.setText(getString(R.string.status_positive));
+        updateButton.setText(getString(R.string.status_positive));
+
+        // Setup queue question view
+        queueView = (TextView) findViewById(R.id.currentQuestion);
+        queueView.setText("");
 
 
         // Input Question
@@ -105,6 +113,16 @@ public class MainActivity extends Activity {
 
             }
         });
+
+        // Swipey thing
+        mDetector = new GestureDetector(this, this);
+        relativeLayout.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent me) {
+                mDetector.onTouchEvent(me);
+
+                return mDetector.onTouchEvent(me);
+            }
+        });
     }
 
     // runs after LoginActivity, and starts the constant data fetching functions. Also sets up button
@@ -118,6 +136,7 @@ public class MainActivity extends Activity {
                     getConfusionData();
                     getQuestionData();
                     deleteOldQuestions();
+                    updateQuestionQueue();
                     buttonClicked();
                 }
                 break;
@@ -147,13 +166,17 @@ public class MainActivity extends Activity {
                             // retrieve success
                             if (confused) {
                                 object.increment("current");
-                                relativeLayout.setBackgroundColor(RED);
-                                status.setText(getString(R.string.status_negative));
+                                //relativeLayout.setBackgroundColor(RED);
+                                updateButton.setText(getString(R.string.status_negative));
+                                updateButton.setBackgroundColor(RED);
+                                updateButton.setTextColor(Color.parseColor("#FFFFFF"));
 
                             } else {
                                 object.increment("current", -1);
-                                relativeLayout.setBackgroundColor(GREEN);
-                                status.setText(getString(R.string.status_positive));
+                                //relativeLayout.setBackgroundColor(GREEN);
+                                updateButton.setText(getString(R.string.status_positive));
+                                updateButton.setBackgroundColor(GREEN);
+                                updateButton.setTextColor(Color.parseColor("#999999"));
                             }
 
                             confused = !confused;
@@ -206,7 +229,7 @@ public class MainActivity extends Activity {
                             //post to screen
                             ArrayList<String> questions = new ArrayList<String>();
                             for (int i = 0; i < scoreList.size(); i++) {
-                                questions.add(i+1+ ". " + scoreList.get(i).getString("question"));
+                                questions.add(scoreList.get(i).getString("question"));
                             }
 
                             postTopQuestions(questions);
@@ -255,6 +278,48 @@ public class MainActivity extends Activity {
 
     }
 
+    public void updateQuestionQueue() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            public void run() {
+
+                ParseQuery<ParseObject> query = ParseQuery.getQuery(class_name + "Questions");
+                query.orderByAscending("createdAt");
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> scoreList, ParseException e) {
+                        if (e == null) {
+                            for (int i = 0; i < scoreList.size(); i++) {
+
+                                Date date = scoreList.get(i).getCreatedAt();
+
+                                if (lastDate.before(date))
+                                {
+                                    Question_queue.add(new String[]{scoreList.get(i).getString("question"),
+                                            scoreList.get(i).getObjectId()});
+                                }
+                            }
+                            if (!scoreList.isEmpty()) {
+                                lastDate = scoreList.get(scoreList.size() - 1).getCreatedAt();
+                                if ((queueView.getText().equals("No more new questions!") ||
+                                        queueView.getText().equals("")) && !Question_queue.isEmpty())
+                                {
+                                    queueView.setText(Question_queue.get(0)[0]);
+                                }
+                            }
+
+                        } else {
+                            Log.d("score", "Error: " + e.getMessage());
+                        }
+                    }
+                });
+                handler.postDelayed(this, mInterval);
+            }
+        }, mInterval);
+
+    }
+
+
     public void postTopQuestions(ArrayList<String> questions) {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
                         (this, android.R.layout.simple_list_item_1, questions);
@@ -265,6 +330,85 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent arg0) {
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                           float velocityY) {
+        float sensitvity = 1;
+        if (!Question_queue.isEmpty()) {
+            if ((e1.getX() - e2.getX()) > sensitvity && e1.getY() > queueView.getTop() - 150 && e2.getY() > queueView.getTop() - 150) {
+                //left fling
+                if(e1.getY()<queueView.getTop()+queueView.getHeight()+150 && e2.getY()<queueView.getTop()+queueView.getHeight()+150) {
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery(class_name + "Questions");
+                    query.whereEqualTo("objectId", Question_queue.get(0)[1]);
+                    query.getFirstInBackground(new GetCallback<ParseObject>() {
+                        @Override
+                        public void done(ParseObject object, ParseException e) {
+                            if (object == null) {
+                                //failed
+                            } else {
+                                // retrieve success
+                                object.increment("votes", -1);
+                                object.saveInBackground();
+                            }
+                        }
+                    });
+                    queueView.setText(Question_queue.get(0)[0]);
+                    Question_queue.remove(0);
+                }
+            } else if ((e2.getX() - e1.getX()) > sensitvity && e1.getY() > queueView.getTop() - 150 && e2.getY() > queueView.getTop() - 150) {
+                //right fling
+                if(e1.getY()<queueView.getTop()+queueView.getHeight() +150 && e2.getY()<queueView.getTop()+queueView.getHeight()+150) {
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery(class_name + "Questions");
+                    query.whereEqualTo("objectId", Question_queue.get(0)[1]);
+                    query.getFirstInBackground(new GetCallback<ParseObject>() {
+                        @Override
+                        public void done(ParseObject object, ParseException e) {
+                            if (object == null) {
+                                //failed
+                            } else {
+                                // retrieve success
+                                object.increment("votes");
+                                object.saveInBackground();
+                            }
+                        }
+                    });
+                    queueView.setText(Question_queue.get(0)[0]);
+                    Question_queue.remove(0);
+                }
+            }
+        }
+        else {
+            queueView.setText("No more new questions!");
+        }
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
         return true;
     }
 
